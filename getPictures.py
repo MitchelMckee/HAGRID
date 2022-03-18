79a02f20bc2d671aedcbd7b74473eeb04ad05d02
Mitchel Mckee <mitcheljmckee1507@gmail.com>
2022-03-18 15:36:19 +0000

    .

---
+++ getPictures.py
@@ -0,0 +1,41 @@
+import os
+import cv2 as cv
+from natsort import natsorted, ns    
+
+capture = cv.VideoCapture(0)
+path = '/Users/mitchelmckee/Desktop/screenshots/'
+
+def newFileName():
+    folder = os.fsencode(path)
+    filenames = []
+    global filename
+
+    for file in os.listdir(folder):
+        filename = os.fsdecode(file)
+        if filename.endswith(('.png')):
+            filenames.append(filename)
+
+    filenames = (natsorted(filenames))
+    filename = filenames[-1]
+    filename.split('.')
+    x = int(filename[0]) + 1
+    filename = str(x) + '.png'
+
+def screenshot():
+    print('SCREENSHOT')
+    newFileName()
+    cv.imwrite(path + filename,capture.read()[1]) # or saves it to disk
+
+def runCam():
+    while True:
+        isTrue,frame = capture.read()
+        cv.imshow('Video',frame)
+        if cv.waitKey(20) & 0xFF==ord('d'):
+            break
+        if cv.waitKey(20) & 0xFF==ord('s'):
+            screenshot()
+
+    capture.release()
+    cv.destroyAllWindows()
+
+runCam()
