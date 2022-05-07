04e13d4b47bc15c44d7dabdbb052dc0606b783d2
MitchelMckee <79174382+MitchelMckee@users.noreply.github.com>
2022-05-07 23:09:11 +0100

    bedtime

--- getPictures.py
+++ getPictures.py
@@ -5,6 +5,7 @@ from natsort import natsorted
 
 capture = cv2.VideoCapture(0)
 path = './screenshots/'
+global filename
 
 def newFileName():
     folder = os.fsencode(path)
@@ -28,11 +29,13 @@ def screenshot():
     cv2.imwrite(path + filename,capture.read()[1])
 
 def runCam():
+
+    
     while True:
         (ret, frame) = capture.read()
         grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         (thresh, blackAndWhiteFrame) = cv2.threshold(grayFrame, 127, 255, cv2.THRESH_BINARY)
- 
+    
         isTrue, frame = capture.read()
         cv2.imshow('Video', blackAndWhiteFrame)
         if cv2.waitKey(20) & 0xFF==ord('d'):
@@ -44,20 +47,27 @@ def runCam():
     cv2.destroyAllWindows()
 
 def boundLetter(img):
-    image = cv2.imread(img)
+    image = cv2.imread('./screenshots/' + img)
 
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-
+    ROI_number = 0
     cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 
     for c in cnts:
         x, y, w, h = cv2.boundingRect(c)
         cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        cv2.imwrite('1.png')
 
     cv2.imshow('Image', image)
     cv2.waitKey()
     
-boundLetter("./dataset/a/img037-035.png")
-runCam()
+def takeSSandCreateBoundingBoxes():
+    runCam()
+    print(filename)
+    print("Bounding")
+    boundLetter(filename)
+#runCam()
+#boundLetter()
+takeSSandCreateBoundingBoxes()
