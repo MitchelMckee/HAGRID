d50b3103f32bc08716b53030b9bd8f0071574be7
MitchelMckee <79174382+MitchelMckee@users.noreply.github.com>
2022-05-09 18:00:24 +0100

    functioniseiate

--- brain.py
+++ brain.py
@@ -1,130 +1,96 @@
-from __future__ import print_function
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.losses import categorical_crossentropy
+from keras.utils import np_utils
 
 import keras
-from sklearn import metrics
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, Activation
-from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import BatchNormalization
-from keras.losses import categorical_crossentropy
-from keras import backend as K
-from keras.utils import np_utils
-from keras.datasets import mnist
-
-import pickle
 import numpy as np
 import cv2
 import os
 import random
 import matplotlib.pyplot as plt
 
-
-training_data = []
-
+# PARAMS AND GLOBALS
 IMG_FOLDER = r'./dataset/' 
 IMG_SIZE = 50
-
-CATEGORIES = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-#, 'C_A', 'C_B', 'C_C', 'C_D', 'C_E', 'C_F', 'C_G', 'C_H', 'C_I', 'C_J', 'C_K', 'C_L', 'C_M', 'C_N', 'C_O', 'C_P', 'C_Q', 'C_R', 'C_S', 'C_T', 'C_U', 'C_V', 'C_W', 'C_X', 'C_Y', 'C_Z']
-#CATEGORIES = ['a', 'b']
-
 input_shape = (IMG_SIZE, IMG_SIZE, 1)
 num_classes = 26
 batch_size = 26
-epochs = 10
+epochs = 12
+training_data = []
 
-for category in CATEGORIES:
-    path = os.path.join(IMG_FOLDER, category)
-    for img in os.listdir(path):
-        img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
-        plt.imshow(img_array)
-        break
-    break
-new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE)) # CHANGE ALL IMAGES TO IMG_SIZE
+CATEGORIES = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def create_training_data():
     for category in CATEGORIES:
+        path = os.path.join(IMG_FOLDER, category)
         class_num = CATEGORIES.index(category)
+
         for img in os.listdir(path):
-            img_array = cv2.imread(os.path.join(path,img), cv2.IMREAD_GRAYSCALE)
+            img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
             new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
-            plt.imshow(new_array)
-           # plt.show()
             training_data.append([new_array, class_num])
-           
-    return training_data
 
-create_training_data()
-random.shuffle(training_data)
+def model():
 
-for sample in training_data[:10]:
-    print(sample[1])
+    X = []
+    y = []
 
-X = []
-y = []
+    for features, label in training_data:
+        X.append(features)
+        y.append(label)
 
-for features, label in training_data:
-    X.append(features)
-    y.append(label)
+    X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+    X = X/255.0
 
-X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
-y = np.array(y)
+    y = np.array(y)
+    y = keras.utils.np_utils.to_categorical(y, num_classes)
 
+    model = Sequential()
 
-print(X[1])
-print(y[1])
+    model.add(Conv2D(28, kernel_size=(3, 3), activation="relu", input_shape=input_shape))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+    model.add(Conv2D(56, kernel_size=(3, 3), activation="relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.5))
+    model.add(Flatten())
+    model.add(Dropout(0.5))
+    model.add(Dense(100, activation="relu"))
+    model.add(Dense(num_classes, activation="softmax"))
 
+    model.summary()
 
-#x_test = X[:round(len(training_data)/2)]
-#x_train = X[round(len(training_data)/2):]
-#y_train = y[:round(len(training_data)/2)]
-#y_test = y[round(len(training_data)/2):]
+    model.compile(loss = categorical_crossentropy, optimizer='adam', metrics=['accuracy'])
 
-#x_train = x_train / 255
-#x_test = x_test / 255 
+    model.fit(X, y, batch_size = batch_size, epochs=epochs, verbose=1)
+    score = model.evaluate(X, y, verbose=0)
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
 
-#x_train = x_train.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
-#x_test = x_test.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+    prediction = model.predict([preparePred('./dataset/a/img037-025.png')])
+    pred_name = CATEGORIES[np.argmax(prediction)]
+    print(pred_name)
+    print(prediction)
 
-#y_train = keras.utils.np_utils.to_categorical(y_train, num_classes)
-#y_test = keras.utils.np_utils.to_categorical(y_test, num_classes)
+def preparePred(file):
+    img_array = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+    new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+    return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
 
-'''
-print('-----------------------------------------')
-print(x_train.shape, 'x_train shape')
-print(x_test.shape, 'x_test shape')
-print(x_train[0], 'x_train sample')
-print(x_test[0], 'x_test sample')
-print('-----------------------------------------')
-print(y_train.shape, 'y_train shape')
-print(y_test.shape, 'y_test shape')
-print(y_train[0], 'y_train sample\n')
-print(y_test[0], 'y_test sample')
-print('-----------------------------------------')
-'''
-####################################################################################################################################################
+def main():
+    for category in CATEGORIES:
+        path = os.path.join(IMG_FOLDER, category)
+        for img in os.listdir(path):
+            img_array = cv2.imread(os.path.join(path, img))
 
-model = Sequential()
+    new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
 
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2))) 
-model.add(Dropout(0.9))
-model.add(Flatten()) # This line is to convert from matrices to vectors
-model.add(Dense(26, activation='relu'))
-model.add(Dense(num_classes, activation='softmax')) # we are working with vectors now, so we use a Dense layer instead of Conv2d
-model.summary()
+    create_training_data()
+    random.shuffle(training_data)
 
+    model()
 
-model.compile(loss=categorical_crossentropy,
-              optimizer='adam',
-              metrics=['accuracy'])
+main()
 
-model.fit(X, y,
-          batch_size=batch_size, validation_split=0.1)
-score = model.evaluate(X, y, verbose=0)
-
-
-print(np.argmax(model.predict(X[:1]), axis=-1))
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
--- getPictures.py
+++ getPictures.py
@@ -1,13 +1,13 @@
 import os
 import cv2
 import numpy as np
-from natsort import natsorted
+from natsort import natsorted # USED TO SORT THE FILES IN NATURAL ORDER 
 
 capture = cv2.VideoCapture(0)
 path = './screenshots/'
 global filename
 
-def newFileName():
+def newFileName(): # GET THE NEXT NUMBER AVAILABLE IN THE FILE, THE NAME OF THE FILE WILL ITERATE
     folder = os.fsencode(path)
     filenames = []
     global filename
@@ -23,14 +23,12 @@ def newFileName():
     x = int(filename[0]) + 1
     filename = str(x) + '.png'
 
-def screenshot():
+def screenshot(): # TAKE A SCREENSHOT FROM THE CAMERA
     print('SCREENSHOT')
     newFileName()
     cv2.imwrite(path + filename,capture.read()[1])
 
-def runCam():
-
-    
+def runCam(): # RUN THE CAMERA TO SEE WHAT YOU ARE TRYING TO SCAN
     while True:
         (ret, frame) = capture.read()
         grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -46,9 +44,9 @@ def runCam():
     capture.release()
     cv2.destroyAllWindows()
 
-def boundLetter(img):
-    image = cv2.imread('./predict/Untitled.png')
-   # image = cv2.imread('./screenshots/' + img)
+def boundLetter(img): # CREATE BOUNDING BOXES
+   #image = cv2.imread('./predict/*.png') # CHANGE ASTERISK TO PREDICTION FILE IF YOU ALREADY HAVE A PICTURE
+    image = cv2.imread('./screenshots/' + img) # USE CAMERA AS IMAGE TO PREDICT
     original = image.copy()
 
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -57,24 +55,21 @@ def boundLetter(img):
     cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 
-    for c in cnts:
+    for c in cnts: # CREATE BOUNDING BOXES AND SAVE EACH BOX AS AN IMAGE FILE
         x,y,w,h = cv2.boundingRect(c)
-        cv2.rectangle(image, (x, y), (x + w, y + h), (0,0,255), 2)
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0,0,255), 2) # DRAW A RECTANGLE AROUND EACH SHAPE FOUND
         ROI = original[y:y+h, x:x+w]
-        cv2.imwrite('./predict/Image_{}.png'.format(ROI_number), ROI)
+        cv2.imwrite('./predict/Image_{}.png'.format(ROI_number), ROI) # SAVES EACH BOX AS AN IMAGE WITH AN ITERATING NAME
         ROI_number += 1
 
     cv2.imshow('Image', image)
     cv2.imwrite('./bounded/' + filename, image)
     cv2.waitKey()
     
-def takeSSandCreateBoundingBoxes():
+def takeSSandCreateBoundingBoxes(): # TAKE A SCREENSHOT FROM THE CAMERA, SAVE IT, GIVE IT BOUNDING BOXES AND SAVE THOSE BOXES
     runCam()
     print(filename)
     print("Bounding")
     boundLetter(filename)
 
-    
-#runCam()
-#boundLetter()
 takeSSandCreateBoundingBoxes()
